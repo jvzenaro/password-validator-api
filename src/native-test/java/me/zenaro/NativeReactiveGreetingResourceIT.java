bd526180792a32145871d1c6401a8a3941b1710b
João Zenaro <joao@zenaro.me>
2021-10-31 10:26:44 -0300

    Remove codigos de exemplo

--- src/native-test/java/me/zenaro/NativeReactiveGreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package me.zenaro;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeReactiveGreetingResourceIT extends ReactiveGreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
