3bef3405b8ffd59c700912c2692e7d7b2dd04311
João Zenaro <joao@zenaro.me>
2021-10-31 11:15:27 -0300

    Cria validacao de tamanho minimo

---
+++ src/main/java/me/zenaro/validator/MinSizeValidation.java
@@ -0,0 +1,20 @@
+package me.zenaro.validator;
+
+import javax.inject.Singleton;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Singleton
+public class MinSizeValidation implements IValidation<String> {
+
+    private final Integer minSize;
+
+    public MinSizeValidation(@ConfigProperty(name = "validator.size.min") final Integer minSize) {
+        this.minSize = minSize;
+    }
+
+    public boolean validate(final String value) {
+        return value != null && value.length() >= minSize;
+    }
+
+}
